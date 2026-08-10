d2923aeb010dc91a7c68f3ab2b69fa83ea8e77c2
Alex <mdllvr.web@gmail.com>
2026-08-10 15:37:41 +0300

    slider arrows fix

--- src/components/TestimonialsSlider/index.tsx
+++ src/components/TestimonialsSlider/index.tsx
@@ -11,6 +11,25 @@ import { testimonials } from "@/lib/site-data";
 import "swiper/css";
 import "swiper/css/effect-fade";
 
+const quoteArrowPath =
+  "M0.292892 6.65686C-0.0976257 7.04739 -0.0976257 7.68055 0.292892 8.07107L6.65685 14.435C7.04738 14.8256 7.68054 14.8256 8.07107 14.435C8.46159 14.0445 8.46159 13.4113 8.07107 13.0208L2.41422 7.36397L8.07107 1.70711C8.46159 1.31659 8.46159 0.683424 8.07107 0.2929C7.68054 -0.0976243 7.04738 -0.0976243 6.65685 0.2929L0.292892 6.65686ZM129 7.36397V6.36397L1 6.36397V7.36397V8.36397L129 8.36397V7.36397Z";
+
+function QuoteArrow({ flipped = false }: { flipped?: boolean }) {
+  return (
+    <svg
+      width="129"
+      height="15"
+      viewBox="0 0 129 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      className={flipped ? styles.quoteArrowFlipped : undefined}
+    >
+      <path d={quoteArrowPath} fill="currentColor" />
+    </svg>
+  );
+}
+
 export function TestimonialsSlider() {
   const [swiper, setSwiper] = useState<SwiperType | null>(null);
 
@@ -51,15 +70,15 @@ export function TestimonialsSlider() {
           aria-label="Предыдущий отзыв"
           onClick={() => swiper?.slidePrev()}
         >
-          <i aria-hidden="true" />
+          <QuoteArrow />
         </button>
         <button
           type="button"
-          className={styles.quoteControl}
+          className={`${styles.quoteControl} ${styles.quoteControlNext}`}
           aria-label="Следующий отзыв"
           onClick={() => swiper?.slideNext()}
         >
-          <i aria-hidden="true" />
+          <QuoteArrow flipped />
         </button>
       </div>
     </blockquote>
